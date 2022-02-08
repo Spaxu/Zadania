0b4b0f2653cb039138ec54e930a97ee46f139895
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-08 13:35:30 +0100

    L2Z3

---
+++ src/main/java/L2Z4/AgeCheck.java
@@ -0,0 +1,8 @@
+package L2Z4;
+
+public class AgeCheck {
+    public static boolean ageChecker(int age) {
+        return age >= 18;
+    }
+
+}
---
+++ src/test/java/AgeCheckTest/L2Z4Test.java
@@ -0,0 +1,19 @@
+package AgeCheckTest;
+
+import L2Z4.AgeCheck;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class L2Z4Test {
+    @Test
+    void shouldTrueIfAdult() {
+        //given
+        int age = 18;
+
+        //when
+        boolean adult = AgeCheck.ageChecker(age);
+
+        //then
+        Assertions.assertTrue(adult);
+    }
+}
