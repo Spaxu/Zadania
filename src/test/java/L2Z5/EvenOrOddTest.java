bdf624db26592308d3de3545e21739a67d216da9
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-08 13:49:48 +0100

    L2Z5

---
+++ src/test/java/L2Z5/EvenOrOddTest.java
@@ -0,0 +1,18 @@
+package L2Z5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EvenOrOddTest {
+    @Test
+    void shouldTrueIfNumberIsEven() {
+        //given
+        int number = 4;
+
+        //when
+        boolean x = EvenOrOdd.checkIfEven(number);
+
+        //then
+        Assertions.assertTrue(x);
+    }
+}
