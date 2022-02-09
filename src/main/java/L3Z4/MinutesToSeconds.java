ce37460f81b4aa1b98ffd1fc7b32bae1317af590
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-09 15:07:51 +0100

    L3Z4

--- src/main/java/L3Z4/MinutesToSeconds.java
+++ src/main/java/L3Z4/MinutesToSeconds.java
@@ -2,6 +2,8 @@ package L3Z4;
 
 public class MinutesToSeconds {
     public static int convertMinutesToSeconds(int minutes){
+
         return minutes * 60;
+
     }
 }
--- src/test/java/L3Z4/MinutesToSecondsTest.java
+++ src/test/java/L3Z4/MinutesToSecondsTest.java
@@ -7,12 +7,12 @@ public class MinutesToSecondsTest {
     @Test
     void shouldAddTwoValues1and1() {
         //given
-        int minutes = 10;
+        int minutes = 9;
 
         //when
         int seconds = MinutesToSeconds.convertMinutesToSeconds(minutes);
 
         //then
-        Assertions.assertEquals(600, seconds);
+        Assertions.assertEquals(540, seconds);
     }
 }
