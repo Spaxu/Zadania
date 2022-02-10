65a84f61cf5300d1f485c1c898a6c9d27f8df5de
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-10 22:29:49 +0100

    L3Z2 - poprawa 2

--- src/main/java/L3Z2/Movie.java
+++ src/main/java/L3Z2/Movie.java
@@ -1,17 +1,17 @@
 package L3Z2;
 
 public class Movie {
-    final String title;
-    final String directorName;
-    final String directorSurname;
-    final int durationInMinutes;
-    final int prodYear;
+    private final String title;
+    private final String directorName;
+    private final String directorSurname;
+    private final int durationInMinutes;
+    private final int prodYear;
 
 
-    public Movie(String title, String directorName, String directorSurame, int durationInMinutes, int prodYear) {
+    public Movie(String title, String directorName, String directorSurname, int durationInMinutes, int prodYear) {
         this.title = title;
         this.directorName = directorName;
-        this.directorSurname = directorSurame;
+        this.directorSurname = directorSurname;
         this.durationInMinutes = durationInMinutes;
         this.prodYear = prodYear;
     }
