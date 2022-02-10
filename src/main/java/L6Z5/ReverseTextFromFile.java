1a8e7aa1fc97b33efad360f26fa48fe7d1ffacf0
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-11 00:58:49 +0100

    L6Z5

---
+++ src/main/java/L6Z5/ReverseTextFromFile.java
@@ -0,0 +1,24 @@
+package L6Z5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReverseTextFromFile {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader reader =
+                    Files.newBufferedReader(Paths.get("C:\\Users\\Spax\\Desktop\\data.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(new StringBuilder(line).reverse());
+
+            }
+        } catch (
+                IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
