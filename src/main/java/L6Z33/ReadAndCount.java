258d1bf838dd248620a29b56978ca1c51eebefea
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-11 00:23:06 +0100

    L6Z3

---
+++ src/main/java/L6Z33/ReadAndCount.java
@@ -0,0 +1,26 @@
+package L6Z33;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadAndCount {
+    public static void main(String[] args) throws IOException {
+        try {
+            BufferedReader reader =
+                    Files.newBufferedReader(Paths.get("C:\\Users\\Spax\\Desktop\\data.txt"));
+            int charCount = 0;
+            String data;
+            while((data = reader.readLine()) != null) {
+                charCount += data.length();
+            }
+            System.out.println(charCount);
+        } catch (
+                IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+}
+
