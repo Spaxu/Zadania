667e3cf700ab55fe9bd6c7c53ac79f5b8c373ef4
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-10 23:41:03 +0100

    L5Z3

---
+++ src/main/java/L5Z3/BetterCalculator.java
@@ -0,0 +1,18 @@
+package L5Z3;
+
+public class BetterCalculator {
+
+    public static int sumOfTwoNumbers(int value1, int value2) {
+        return value1 + value2;
+    }
+    public static int subtractionOfTwoNumbers(int value1, int value2){
+        return value1 - value2;
+    }
+    public static int multiplicationOfTwoNumbers(int value1, int value2){
+        return value1 * value2;
+    }
+    public static int dividingOfTwoNumbers(int value1, int value2){
+        return value1 / value2;
+    }
+
+}
