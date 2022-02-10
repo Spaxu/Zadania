56908479dce309e775c3d899957176ea1bb7aa98
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-10 23:04:03 +0100

    L4Z7

---
+++ src/main/java/L4Z7/DayOfTheWeek.java
@@ -0,0 +1,11 @@
+package L4Z7;
+
+public enum DayOfTheWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/L4Z7/Days.java
@@ -0,0 +1,17 @@
+package L4Z7;
+
+import java.util.Locale;
+
+public class Days{
+
+    public static void main(String[] args) {
+        System.out.println(DayOfTheWeek.MONDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.TUESDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.WEDNESDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.THURSDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.FRIDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.SATURDAY.name().toLowerCase());
+        System.out.println(DayOfTheWeek.SUNDAY.name().toLowerCase());
+
+    }
+}
