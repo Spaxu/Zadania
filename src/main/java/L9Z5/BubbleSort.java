410ba179eabd498cdf78543acc6445c2fdc8a001
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-11 02:06:16 +0100

    L9Z5

---
+++ src/main/java/L9Z5/BubbleSort.java
@@ -0,0 +1,27 @@
+package L9Z5;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] test = {9, 5, 2, 7, 1};
+
+        bubble_sort(test);
+        for (int a = 0; a < test.length; a++) {
+            System.out.print(test[a] + " ");
+        }
+    }
+
+    public static void bubble_sort(int[] tab) {
+        int tmp;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    tmp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = tmp;
+                }
+
+            }
+        }
+
+    }
+}
