a69d3534c844e59c028e504caaf06173d0a9a255
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-11 10:21:49 +0100

    L6Z2 - poprawa

--- src/main/java/L6Z2/ReadFromFile.java
+++ src/main/java/L6Z2/ReadFromFile.java
@@ -2,13 +2,14 @@ package L6Z2;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReadFromFile {
 
     public static void main(String[] args) {
         try {
-            String content = Files.readString(Paths.get("C:\\Users\\Spax\\Desktop\\data.txt"));
+            String content = Files.readString(Path.of(System.getProperty("user.home") + "\\Desktop\\data.txt"));
             System.out.println(content);
         } catch (
                 IOException exception) {
