85fdd4a83faafecc058d5844edefcc31edbf4d25
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-11 09:59:48 +0100

    L4Z7 - poprawa

--- src/main/java/L4Z7/Days.java
+++ src/main/java/L4Z7/Days.java
@@ -2,16 +2,10 @@ package L4Z7;
 
 import java.util.Locale;
 
-public class Days{
+public class Days {
 
-    public static void main(String[] args) {
-        System.out.println(DayOfTheWeek.MONDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.TUESDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.WEDNESDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.THURSDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.FRIDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.SATURDAY.name().toLowerCase());
-        System.out.println(DayOfTheWeek.SUNDAY.name().toLowerCase());
-
+    public String enumToString(DayOfTheWeek day) {
+        return day.name();
     }
+
 }
