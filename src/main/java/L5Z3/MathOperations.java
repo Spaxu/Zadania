ecb5b5a20aad46b4d5270c9fcf11dcd8d4bd1b53
Patryk Piotrowski <patrykpiotrowski1996@gmail.com>
2022-02-12 08:49:00 +0100

    L5Z3

--- src/main/java/L5Z3/MathOperations.java
+++ src/main/java/L5Z3/MathOperations.java
@@ -2,10 +2,8 @@ package L5Z3;
 
 public interface MathOperations {
 
-    int sumOfTwoNumbers(int value1, int value2);
-    int subtractionOfTwoNumbers(int value1, int value2);
-    int multiplicationOfTwoNumbers(int value1, int value2);
-    int dividingOfTwoNumbers(int value1, int value2);
+    int calculateTwoNumbers(int value1, int value2);
+
 
 
 }
